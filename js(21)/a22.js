65589b1db957aa5ec34caf0d523ffa3017f29a9b
basskey19 <rohann@gmail.com>
2025-08-25 18:23:01 +0530

    substitue

---
+++ js(21)/a22.js
@@ -0,0 +1,14 @@
+s = {
+    name: "s.sarvesh",
+    age: 13,
+    skills: "good at making joke "
+
+}
+friends = [ s, "a", "b", "c"]
+console.log(friends)
+console.log(friends[1])
+console.log(friends[0].skills)
+
+console.log(friends.length)
+console.log(friends.push("d"))
+console.log(friends.pop())
